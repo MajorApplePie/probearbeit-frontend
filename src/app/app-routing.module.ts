f818eac5be447f293bc4e3eca64c15cbfae38593
Marcel Bähr <marcel.baehr.127@gmail.com>
2020-08-27 09:20:48 +0200

    Impl: default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'converter', loadChildren: () => import('./modules/converter/converter.module').then(m => m.ConverterModule) }, { path: 'chart', loadChildren: () => import('./modules/chart/chart.module').then(m => m.ChartModule) }, { path: 'wallet', loadChildren: () => import('./modules/wallet/wallet.module').then(m => m.WalletModule) }, { path: 'details', loadChildren: () => import('./modules/details/details.module').then(m => m.DetailsModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'converter',
+    loadChildren: () => import('./modules/converter/converter.module').then(m => m.ConverterModule)
+  },
+  {
+    path: 'chart',
+    loadChildren: () => import('./modules/chart/chart.module').then(m => m.ChartModule)
+  },
+  {
+    path: 'wallet',
+    loadChildren: () => import('./modules/wallet/wallet.module').then(m => m.WalletModule)
+  },
+  {
+    path: 'details',
+    loadChildren: () => import('./modules/details/details.module').then(m => m.DetailsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
